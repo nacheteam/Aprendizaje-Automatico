a9d7f876896c3b5a24adfe439d890135aa67d65a
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-03-05 21:47:57 +0100

    Hacemos el cálculo exacto

--- Practica 0/main.py
+++ Practica 0/main.py
@@ -79,17 +79,48 @@ def parte2():
     # Calculamos el tamaño del test y train
     test_size = int(test_percentage*len(data))
     train_size = len(data)-test_size
-    # Calculamos números de forma aleatoria (según una uniforme) de para sacar los índices
-    # del conjunto de test
-    test_index = np.random.choice(len(data), test_size,replace=False)
-    # Calculamos los índices del conjunto de entrenamiento como los índices que no hemos
-    # empleado en el conjunto de test
-    train_index = np.array(list(set([i for i in range(len(data))]).difference(set(test_index))))
+
+    #Dividimos los datos por clases, esto es una lista con sublistas que contienen en cada
+    # posición los elementos de la clase correspondiente, es decir [[elementos_clase0], [elementos_clase1], [elementos_clase2]]
+    max_label = max(labels)
+    data_divided = []
+    for i in range(max_label+1):
+        subsample = []
+        for j in range(len(data)):
+            if labels[j]==i:
+                # Lo pongo como una lista para que se vea mejor cuando lo imprima
+                subsample.append(list(data[j]))
+        # Antes de añadir la sublista la barajamos para obtener una muestra aleatoria
+        np.random.shuffle(subsample)
+        data_divided.append(subsample)
+    # Obtenemos el porcentaje de aparición de cada clase en el conjunto de datos original
+    classes_percentage = [len(data_divided[i])/len(data) for i in range(len(data_divided))]
+    # Obtenemos el número de elementos de cada clase que corresponden para el test
+    elem_per_class_test = [int(test_size*classes_percentage[i]) for i in range(len(classes_percentage))]
+
     # Obtenemos los conjuntos de datos de train y test
-    test_data = data[test_index]
-    train_data = data[train_index]
-    test_labels = labels[test_index]
-    train_labels = labels[train_index]
+    test_data = []
+    train_data = []
+    # Nos quedamos para el test con los primeros elementos (en este caso los 10 primeros)
+    # aprovechandonos del hecho de que hemos barajado previamente cada una de las sublistas
+    for i in range(len(data_divided)):
+        test_data = test_data+data_divided[i][:elem_per_class_test[i]]
+        train_data = train_data+data_divided[i][elem_per_class_test[i]:]
+    # Obtenemos las etiquetas de train y test
+    test_labels = []
+    train_labels = []
+    # Generamos una lista que contiene las etiquetas de test y train, en este caso al haber
+    # 10 instancias de cada clase en el conjunto de test este bucle lo que hace es concatenar los
+    # vectores [0,...,0]+[1,...,1]+[2,...,2] siendo los 3 de longitud 10.
+    for i in range(len(elem_per_class_test)):
+        test_labels = test_labels+(elem_per_class_test[i]*[i])
+        train_labels = train_labels+(len(data_divided[i])-elem_per_class_test[i])*[i]
+
+    # Imprimimos los conjuntos de datos de test y train
+    print("Datos de test")
+    print(test_data)
+    print("\n\n\nDatos de train")
+    print(train_data)
 
     # Contamos los elementos que hay en cada clase para el conjunto de test y train
     # y lo mostramos por pantalla
@@ -105,16 +136,14 @@ def parte2():
     # Calculamos los porcentajes obtenidos en el train y en el test
     # Sabemos que la proporción de datos es de 50,50,50 para las clases, esto es un 33% de aparición
     # de cada clase
-    # Los porcentajes no van a salir exactamente un 33% porque me he valido del uso de la probabilidad.
-    # Se emplea el hecho de que, si el número de datos creciera de forma indefinida entonces
-    # al tomar un conjunto de elementos mediante una distibución unifome obtendríamos el mismo número
-    # de elementos de cada clase, esto es un 33% de cada una de las clases.
+    # Los porcentajes salen exactamente un 33% porque el cálculo es exacto
     print("Porcentaje de clases en test")
     print(dict(zip(unique_test, counts_test/np.sum(counts_test))))
     print("Porcentaje de clases en train")
     print(dict(zip(unique_train, counts_train/np.sum(counts_train))))
 
     input("Pulsa ENTER para seguir")
+
 
 ################################################################################
 ##                                   Parte 3                                  ##
