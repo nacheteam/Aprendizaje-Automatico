d255e4c9cc7266a2a348694189c858e57f56ab89
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-03-11 19:05:31 +0100

    Generaliza para cualquier funcion de cualquier numero de variables

--- Practica 1/codigo/practica1.py
+++ Practica 1/codigo/practica1.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from sympy import *
 
 np.random.seed(1)
 
@@ -12,31 +13,29 @@ np.random.seed(1)
 #------------------------------------------------------------------------------#
 ##                              Apartado 1                                    ##
 #------------------------------------------------------------------------------#
+def evaluate(E,symbols,w):
+    value = E.subs(symbols[0],w[0])
+    for j in range(1,len(symbols)):
+        new_value = value.subs(symbols[j],w[j])
+        value = new_value
+    return N(value)
 
-# Función de la que queremos hallar el mínimo.
-def E(u,v):
-    return (u**2*np.exp(v)-2*v**2*np.exp(-u))**2
+def gradiente(E,symbols,w):
+    gradiente = []
+    for i in range(len(symbols)):
+        dife = diff(E,symbols[i])
+        gradiente.append(evaluate(dife,symbols,w))
 
-# Derivada parcial de E con respecto a u
-def dEu(u,v):
-    return 2*np.sqrt(E(u,v))*(2*u*np.exp(v)+2*v**2*np.exp(-u))
-
-# Derivada parcial de E con respecto a v
-def dEv(u,v):
-    return 2*np.sqrt(E(u,v))*(u**2*np.exp(v)-4*v*np.exp(-u))
-
-# Gradiente de E
-def gradE(u,v):
-    return np.array([dEu(u,v), dEv(u,v)])
+    return np.array(gradiente)
 
 # Implementación del método gradiente descendente
-def gradient_descent(w_init,learning_rate,max_iter,tol):
+def gradient_descent(w_init,learning_rate,max_iter,tol,E,symbols):
     w_before=w_init
-    w = w_before-learning_rate*gradE(w_before[0],w_before[1])
+    w = w_before-learning_rate*gradiente(E,symbols,w_before)
     iterations=1
-    while iterations<max_iter and np.linalg.norm(E(w[0],w[1])-E(w_before[0],w_before[1]))>=tol:
+    while iterations<max_iter and np.absolute(evaluate(E,symbols,w)-evaluate(E,symbols,w_before))>=tol:
         w_before=w
-        w = w_before-learning_rate*gradE(w_before[0],w_before[1])
+        w = w_before-learning_rate*gradiente(E,symbols,w_before)
         iterations+=1
     return w, iterations
 
@@ -49,10 +48,20 @@ def apartado2():
     maxIter = 10000000000
     error2get = 1e-14
     initial_point = np.array([1.0,1.0])
-    w, it = gradient_descent(initial_point,eta,maxIter,error2get)
+    u,v = symbols('u v',real=True)
+    symbol = [u,v]
+    expr = (u**2*exp(v)-2*v**2*exp(-u))**2
+    w, it = gradient_descent(initial_point,eta,maxIter,error2get,expr,symbol)
     print("El número de iteraciones empleado ha sido de: " + str(it))
     print("El mínimo encontrado por gradiente desdendente ha sido: ")
     print("x: (" + str(w[0]) + ")")
     print("y: (" + str(w[1]) + ")")
+
+#------------------------------------------------------------------------------#
+##                               Apartado 3                                   ##
+#------------------------------------------------------------------------------#
+
+def apartado3():
+    print("hola")
 
 apartado2()
