8772974aaad4eacf6c9d244bc88feabd4bdd1e33
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-04-21 17:28:10 +0200

    Comentarios añadidos

--- P2/src/practica2.py
+++ P2/src/practica2.py
@@ -173,13 +173,17 @@ def f4(x):
 
 def ej1ap3():
     print("#################################\nEjercicio 1, apartado 3\n#################################\n\n")
+
+    # Separamos los datos con ruido del apartado anterior por etiquetas
     datosA = np.array([nube_unif[i] for i in range(len(nube_unif)) if labels_con_ruido[i]==-1])
     datosB = np.array([nube_unif[i] for i in range(len(nube_unif)) if labels_con_ruido[i]==1])
 
+    # Hacemos los meshgrid necesarios para la orden contour para pintar funciones de dos variables
     x = np.arange(-50,50,0.1)
     y = np.arange(-50,50,0.1)
     xx,yy = np.meshgrid(x,y)
 
+    # Dibujamos la función 1
     plt.scatter(datosA[:,0], datosA[:,1], c="green", label="Datos con etiqueta -1")
     plt.scatter(datosB[:,0], datosB[:,1], c="red", label="Datos con etiqueta 1")
     plt.contour(x,y,f1([xx,yy]),[0])
@@ -189,6 +193,7 @@ def ej1ap3():
 
     input("Presione ENTER para continuar")
 
+    # Dibujamos la función 2
     plt.scatter(datosA[:,0], datosA[:,1], c="green", label="Datos con etiqueta -1")
     plt.scatter(datosB[:,0], datosB[:,1], c="red", label="Datos con etiqueta 1")
     plt.contour(x,y,f2([xx,yy]),[0])
@@ -198,6 +203,7 @@ def ej1ap3():
 
     input("Presione ENTER para continuar")
 
+    # Dibujamos la función 3
     plt.scatter(datosA[:,0], datosA[:,1], c="green", label="Datos con etiqueta -1")
     plt.scatter(datosB[:,0], datosB[:,1], c="red", label="Datos con etiqueta 1")
     plt.contour(x,y,f3([xx,yy]),[0])
@@ -207,6 +213,7 @@ def ej1ap3():
 
     input("Presione ENTER para continuar")
 
+    # Dibujamos la función 4
     plt.scatter(datosA[:,0], datosA[:,1], c="green", label="Datos con etiqueta -1")
     plt.scatter(datosB[:,0], datosB[:,1], c="red", label="Datos con etiqueta 1")
     plt.contour(x,y,f4([xx,yy]),[0])
@@ -227,30 +234,57 @@ ej1ap3()
 #------------------------------------------------------------------------------#
 
 def ajusta_PLA(datos, label, max_iter, vini):
+    '''
+    @brief Método que implementa el algoritmo PLA
+    @param datos numpy array con los datos
+    @param label etiquetas correspondientes para los datos
+    @param max_iter número máximo de iteraciones (épocas)
+    @param vini Valor inicial del algoritmo para w
+    @return Devuelve el vector de pesos w obtenido que nos da la recta separadora y
+    el número de iteraciones (épocas) empleadas
+    '''
+    # Tomamos el valor inicial
     w = vini
     for i in range(max_iter):
+        # Nos quedamos con el w anterior
         w_old = np.copy(w)
+        # Para cada dato
         for d,l in zip(datos, label):
+            # Si no está bien clasificado entonces actualizamos w
             if np.sign(w.dot(d))!=l:
                 w = w+l*d
+        # Si w no ha cambiado en una pasada completa de los datos
         if np.all(np.equal(w, w_old)):
+            # Devolvemos w y el número de iteraciones
             return w, i
+    # Si hemos agotado el número de iteraciones devolvemos lo obenido
     return w,max_iter
 
 def evaluaRecta(w, punto):
+    '''
+    @brief Función que se encarga de evaluar la recta w en el punto pasado
+    @param w numpy array que representa la recta
+    @param punto es un número real sobre el que vamos a evaluar la recta
+    @return Devuelve un número que es el valor de la recta w en el punto
+    pasado como parámetro
+    '''
     return (-w[0]-w[1]*punto)/w[2]
 
 def ej2ap1SinRuido():
     print("#################################\nEjercicio 2, apartado 1 sin ruido\n#################################\n\n")
+    # Tomamos los datos sin ruido del ejercicio 1 y le colocamos 1s al principio
     nube_sin_ruido3d = np.hstack((np.ones(shape=(len(nube_unif),1)),nube_unif))
 
+    # Ejecutamos el algoritmo PLA sobre los datos con las etiquetas sin ruido empezando en [0,0,0]
     w,it = ajusta_PLA(nube_sin_ruido3d, labels_sin_ruido, 10000, np.array([0,0,0]))
     print("Estamos utilizando w_ini = " + str(np.array([0,0,0])))
     print("El número de iteraciones que ha necesitado ha sido: " + str(it))
 
+    # Dividimos los datos por etiquetas
     nube1 = np.array([nube_sin_ruido3d[i] for i in range(len(nube_sin_ruido3d)) if labels_sin_ruido[i]==1])
     nube2 = np.array([nube_sin_ruido3d[i] for i in range(len(nube_sin_ruido3d)) if labels_sin_ruido[i]==-1])
 
+    # Imprimimos la recta obtenida con PLA empezando en [0,0,0]
     plt.scatter(nube1[:,1],nube1[:,2],c="b",label="Clase con etiqueta 1")
     plt.scatter(nube2[:,1],nube2[:,2],c="g",label="Clase con etiqueta -1")
     plt.plot([-51,51],[evaluaRecta(w,-51),evaluaRecta(w,51)],c="r",label="Recta obtenida por PLA sin ruido con w=[0,0,0]")
@@ -259,11 +293,14 @@ def ej2ap1SinRuido():
 
     input("Presione ENTER para continuar")
 
+    # Generamos 10 vectores inciales aleatorios con valores entre 0 y 1
     for i in range(10):
         w = np.random.uniform(low=0, high=1, size=3)
         print("\nEstamos utilizando w_ini = " + str(w))
+        # Obtenemos la w con la inicial aleatoria y los datos sin ruido
         w,it = ajusta_PLA(nube_sin_ruido3d, labels_sin_ruido, 10000, w)
         print("El número de iteraciones que ha necesitado ha sido: " + str(it))
+        # Pintamos la recta obtenida
         plt.scatter(nube1[:,1],nube1[:,2],c="b",label="Clase con etiqueta 1")
         plt.scatter(nube2[:,1],nube2[:,2],c="g",label="Clase con etiqueta -1")
         plt.plot([-51,51],[evaluaRecta(w,-51),evaluaRecta(w,51)],c="r",label="Recta obtenida por PLA sin ruido " + str(i))
@@ -273,15 +310,19 @@ def ej2ap1SinRuido():
 
 def ej2ap1ConRuido():
     print("#################################\nEjercicio 2, apartado 1 con ruido\n#################################\n\n")
+    # Tomamos los datos con ruido del ejercicio 1 y le colocamos 1s al principio
     nube_con_ruido3d = np.hstack((np.ones(shape=(len(nube_unif),1)),nube_unif))
 
+    # Ejecutamos el algoritmo PLA sobre los datos con las etiquetas con ruido empezando en [0,0,0]
     w,it = ajusta_PLA(nube_con_ruido3d, labels_con_ruido, 10000, np.array([0,0,0]))
     print("Estamos utilizando w_ini = " + str(np.array([0,0,0])))
     print("El número de iteraciones que ha necesitado ha sido: " + str(it))
 
+    # Dividimos los datos por etiquetas
     nube1 = np.array([nube_con_ruido3d[i] for i in range(len(nube_con_ruido3d)) if labels_con_ruido[i]==1])
     nube2 = np.array([nube_con_ruido3d[i] for i in range(len(nube_con_ruido3d)) if labels_con_ruido[i]==-1])
 
+    # Imprimimos la recta obtenida con PLA empezando en [0,0,0]
     plt.scatter(nube1[:,1],nube1[:,2],c="b",label="Clase con etiqueta 1")
     plt.scatter(nube2[:,1],nube2[:,2],c="g",label="Clase con etiqueta -1")
     plt.plot([-51,51],[evaluaRecta(w,-51),evaluaRecta(w,51)],c="r",label="Recta obtenida por PLA con ruido con w=[0,0,0]")
@@ -290,11 +331,14 @@ def ej2ap1ConRuido():
 
     input("Presione ENTER para continuar")
 
+    # Generamos 10 vectores inciales aleatorios con valores entre 0 y 1
     for i in range(10):
         w = np.random.uniform(low=0, high=1, size=3)
         print("\nEstamos utilizando w_ini = " + str(w))
+        # Obtenemos la w con la inicial aleatoria y los datos con ruido
         w,it = ajusta_PLA(nube_con_ruido3d, labels_con_ruido, 10000, w)
         print("El número de iteraciones que ha necesitado ha sido: " + str(it))
+        # Pintamos la recta obtenida
         plt.scatter(nube1[:,1],nube1[:,2],c="b",label="Clase con etiqueta 1")
         plt.scatter(nube2[:,1],nube2[:,2],c="g",label="Clase con etiqueta -1")
         plt.plot([-51,51],[evaluaRecta(w,-51),evaluaRecta(w,51)],c="r",label="Recta obtenida por PLA con ruido " + str(i))
@@ -310,28 +354,66 @@ ej2ap1ConRuido()
 #------------------------------------------------------------------------------#
 
 def regresionLogisticaSGD(num_epocas_max,X,y,tasa_aprendizaje=0.01, tol=0.01):
+    '''
+    @brief Función que implementa el algoritmo SGD orientado a regresión logística
+    @param num_epocas_max Número máximo de épocas
+    @param X numpy array con los datos
+    @param y numpy array con las etiquetas de los datos
+    @param tasa_aprendizaje Tasa de aprendizaje para SGD
+    @param tol Tolerancia que gestiona la parada del algoritmo
+    '''
+    # Obtenemos la dimensión del conjuntos de datos
     dimension = len(X[0])
     data_size = len(X)
+    # Empezamos en w=[0,0,0]
     w = np.zeros(dimension)
+    # Ponemos al principio w_old como [1,1,1]
     w_old = np.ones(dimension)
+    # Inicializamos el número de épocas a 0
     num_epocas = 0
+    # Mientras que no superemos el número máximo de épocas y que la diferencia entre w y w_old sea mayor que la tolerancia
     while num_epocas<num_epocas_max and np.linalg.norm(w_old-w)>=tol:
+        # Actualizamos w_old
         w_old=np.copy(w)
+        # Barajamos los índices
         indexes = np.random.choice(data_size, size=data_size, replace=False)
+        # Para cada índice actualizamos w
         for id in indexes:
             w = w-tasa_aprendizaje*((-y[id]*X[id])/(1+np.exp(y[id]*w.dot(X[id]))))
+        # Sumamos 1 al número de épocas
         num_epocas+=1
 
+    # Devolvemos el número de épocas usadas y el w obtenido
     return w,num_epocas
 
 def puntoSobreRecta(a,b,punto):
+    '''
+    @brief Función que determina si un punto está sobre la recta dada por y=a*x+b
+    @param a Pendiente de la recta
+    @param b Término independiente de la recta
+    @param punto punto de la forma [1,x,y] sobre el que queremos valorar si está o no
+    por encima de la recta
+    @return Devuelve True si el punto está por encima de la recta o en la recta y
+    False si está por debajo.
+    '''
+    # Obtenemos el valor de la recta en el punto x
     valor=punto[1]*a+b
-    if valor>=punto[2]:
+    # Si el valor de la recta es menor que el del punto entonces el punto está por encima
+    if valor<punto[2]:
         return True
     else:
         return False
 
 def Eout(datos, labels, w):
+    '''
+    @brief Función que calcula el error cometido por w en los datos pasados.
+    @param datos Datos sobre los que se va a calcular el error
+    @param labels Etiquetas reales de los datos pasados
+    @param w Recta separadora obtenida
+    @return Devuelve el erorr
+    '''
+    # Calculamos el error según las transparencias de teoría, esto es
+    # 1/N * sum(ln(1+e^(-y_n*w^T*x_n)))
     tam_datos = len(datos)
     error = 0
     for d,l in zip(datos,labels):
@@ -340,19 +422,26 @@ def Eout(datos, labels, w):
 
 def ej2ap2():
     print("#################################\nEjercicio 2, apartado 2\n#################################\n\n")
+    # Simulamos 100 datos y colocamos 1s delante
     puntos_uniforme = np.hstack((np.ones(shape=(100,1)),simula_unif(100,2,[0,2])))
+    # Creamos una recta aleatoria
     a,b = simula_recta([0,2])
+    # Calculamos las etiquetas en función de si los puntos quedan o no por encima de la recta aleatoria
     labels = np.array([1 if puntoSobreRecta(a,b,puntos_uniforme[i]) else -1 for i in range(len(puntos_uniforme))])
+    # Calculamos la estimación de w mediante SGD aplicado a regresión logística
     w,iters = regresionLogisticaSGD(10000, puntos_uniforme, labels)
 
     print("El algoritmo ha convergido en " + str(iters) + " iteraciones")
 
+    # Calculamos el Ein
     error = Eout(puntos_uniforme, labels, w)
     print("El error dentro de la muestra es: " + str(error))
 
+    # Dividimos los datos por clases
     puntos_uniforme1 = np.array([puntos_uniforme[i] for i in range(len(puntos_uniforme)) if labels[i]==1])
     puntos_uniforme2 = np.array([puntos_uniforme[i] for i in range(len(puntos_uniforme)) if labels[i]==-1])
 
+    # Pintamos los datos con la recta aleatoria de frontera para ver que lo estamos haciendo bien y cuál sería una solución óptima
     plt.scatter(puntos_uniforme1[:,1], puntos_uniforme1[:,2],c="blue", label="Puntos con etiqueta 1")
     plt.scatter(puntos_uniforme2[:,1], puntos_uniforme2[:,2],c="green", label="Puntos con etiqueta -1")
     plt.plot([0,2],[b, 2*a+b], c="red", label="Recta frontera")
@@ -361,6 +450,7 @@ def ej2ap2():
 
     input("Presione ENTER para continuar")
 
+    # Pintamos la estimación de RL SGD con los 100 datos
     plt.scatter(puntos_uniforme1[:,1], puntos_uniforme1[:,2],c="blue", label="Puntos con etiqueta 1")
     plt.scatter(puntos_uniforme2[:,1], puntos_uniforme2[:,2],c="green", label="Puntos con etiqueta -1")
     plt.plot([0,2],[evaluaRecta(w,0), evaluaRecta(w,2)], c="red", label="Recta Regresión Logística SGD")
@@ -369,16 +459,22 @@ def ej2ap2():
 
     input("Presione ENTER para continuar")
 
+    # Para el tamaño del conjunto desde 1000 a 5000
     for num_puntos in [1000,2000,3000,4000,5000]:
+        # Calculamos una muestra de num_puntos de tamaño
         puntos_uniforme = np.hstack((np.ones(shape=(num_puntos,1)),simula_unif(num_puntos,2,[0,2])))
+        # Calculamos sus etiquetas
         labels = np.array([1 if puntoSobreRecta(a,b,puntos_uniforme[i]) else -1 for i in range(len(puntos_uniforme))])
 
+        # Calculamos el error cometido
         error = Eout(puntos_uniforme, labels, w)
         print("El error con " + str(num_puntos) + " datos ha sido: " + str(error))
 
+        # Separamos los datos por clases
         puntos_uniforme1 = np.array([puntos_uniforme[i] for i in range(len(puntos_uniforme)) if labels[i]==1])
         puntos_uniforme2 = np.array([puntos_uniforme[i] for i in range(len(puntos_uniforme)) if labels[i]==-1])
 
+        # Pintamos w para el nuevo conjunto de datos generado
         plt.scatter(puntos_uniforme1[:,1], puntos_uniforme1[:,2],c="blue", label="Puntos con etiqueta 1")
         plt.scatter(puntos_uniforme2[:,1], puntos_uniforme2[:,2],c="green", label="Puntos con etiqueta -1")
         plt.plot([0,2],[evaluaRecta(w,0), evaluaRecta(w,2)], c="red", label="Recta Regresión Logística SGD")
