ab5b7cc8193d3f9f3f5b5628b0d07e40ffa3289f
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-05-17 16:26:55 +0200

    Métodos para probar todo lo que nos da sklearn

--- P3/src/optdigits.py
+++ P3/src/optdigits.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn import linear_model
+from sklearn.model_selection import train_test_split
 
 # Inicializamos la semilla
 np.random.seed(123456789)
@@ -37,38 +38,49 @@ def readData(path="./datos/optdigits"):
         labels.append(int(line.split(",")[-1]))
     return np.array(dataset), np.array(labels)
 
-def pruebaMinimosCuadradosRL(dataset, labels):
-    reg = linear_model.LinearRegression().fit(dataset,labels)
-    return reg.score(dataset,labels)
+def pruebaMinimosCuadradosRL(train_data, test_data, train_labels, test_labels):
+    reg = linear_model.LinearRegression().fit(train_data,train_labels)
+    return reg.score(test_data,test_labels)
 
-def pruebaRidge(dataset, labels):
+def pruebaRidge(train_data, test_data, train_labels, test_labels):
     clf=linear_model.Ridge(alpha=0.1)
-    clf.fit(dataset, labels)
-    return clf.score(dataset, labels)
+    clf.fit(train_data, train_labels)
+    return clf.score(test_data, test_labels)
 
-def pruebaLasso(dataset, labels):
+def pruebaLasso(train_data, test_data, train_labels, test_labels):
     reg = linear_model.Lasso(alpha=0.1)
-    reg.fit(dataset, labels)
-    return reg.score(dataset, labels)
+    reg.fit(train_data, train_labels)
+    return reg.score(test_data, test_labels)
 
-def pruebaLARSLasso(dataset, labels):
+def pruebaLARSLasso(train_data, test_data, train_labels, test_labels):
     reg = linear_model.LassoLars(alpha=0.1)
-    reg.fit(dataset, labels)
-    return reg.score(dataset, labels)
+    reg.fit(train_data, train_labels)
+    return reg.score(test_data, test_labels)
 
 # Este da un 96% como los champions
-def pruebaSGDClassifier(dataset, labels):
+def pruebaSGDClassifier(train_data, test_data, train_labels, test_labels):
     clf = linear_model.SGDClassifier(max_iter=10000, tol=1e-6)
-    clf.fit(dataset, labels)
-    return clf.score(dataset, labels)
+    clf.fit(train_data, train_labels)
+    return clf.score(test_data, test_labels)
 
 # Este un 99.71%
-def pruebaLogisticRegression(dataset, labels):
+def pruebaLogisticRegression(train_data, test_data, train_labels, test_labels):
     clf = linear_model.LogisticRegression(max_iter=10000, random_state=0, solver='lbfgs', multi_class='multinomial')
-    clf.fit(dataset, labels)
-    return clf.score(dataset, labels)
+    clf.fit(train_data, train_labels)
+    return clf.score(test_data, test_labels)
+
+def pruebaPassiveAgressive(train_data, test_data, train_labels, test_labels):
+    clf = linear_model.PassiveAggressiveClassifier(max_iter=1000, random_state=0, tol=1e-3)
+    clf.fit(train_data, train_labels)
+    return clf.score(test_data, test_labels)
+
+def pruebaPerceptron(train_data, test_data, train_labels, test_labels):
+    clf = linear_model.Perceptron(tol=1e-3, random_state=0)
+    clf.fit(train_data, train_labels)
+    return clf.score(test_data, test_labels)
 
 
 dataset, labels=readData()
-score = pruebaLogisticRegression(dataset, labels)
+train_data, test_data, train_labels, test_labels = train_test_split(dataset, labels, stratify=labels, train_size=0.2, test_size=0.8)
+score=pruebaPerceptron(train_data, test_data, train_labels, test_labels)
 print(score)
